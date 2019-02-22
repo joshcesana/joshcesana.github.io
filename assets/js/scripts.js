c903e81f8494679dac05e1f3d208ed8df9eae44f
JCesana <joshuacesana@gmail.com>
2019-02-21 21:21:28 -0800

    Scroll effect fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,20 +3,6 @@ $(document).ready(function(){
   $('.sidenav').sidenav();
 });
 
-//parallax hero image
-$(document).ready(function(){
-  $('.parallax').parallax();
-});
-
-// hide nav on landing page only
-$(window).scroll(function() {
-  if ($(this).scrollTop() >= $('#main').offset().top) {
-    $('#navbar-fixed').removeClass("nav-hide");
-  }else {
-    $('#navbar-fixed').addClass("nav-hide");
-  }
-});
-
 // close sidenav on click
 $(document).ready(function(){
     $('.sidenav')
@@ -35,7 +21,7 @@ $(document).ready(function($){
             height: $(window).height()
         });
 	}
-  
+
 	fullscreen();
 
   // Run the function in case of window resize
@@ -44,3 +30,42 @@ $(document).ready(function($){
     });
 
 });
+
+// smooth scrolling via links
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
